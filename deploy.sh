6ef8ff14685e1ba715b02c1552e5fc12893b51c7
ravai <nbkrist.21004515@gmail.com>
2018-09-22 20:38:04 +0000

    Scripts first time commiting

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ENVIRONMENT=$1
+if [ $ENVIRONMENT = "QA" ]; then
+for i in `cat IPs.txt`
+do
+echo "Deploying war file into $i QA Server..."
+sleep 6
+ssh pass -p "weblogic@123" scp target/*.war mahesh@$i:/home/mahesh/apache-tomcat-8.5.32/webapps
+echo "Starting tomcat server in $i QA Server.."
+sshpass -p "weblogic@123" ssh mahesh@$i "JAVA_HOME=/home/mahesh/jdk1.8.0_171" "/home/mahesh/apache-tomcat-8.5.32/bin/startup.sh"
+done
+echo "Deployment is successfull"
+fi
