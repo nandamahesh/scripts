90c064b59c4fed95d9639694daa17dd58a280b31
ravai <nbkrist.21004515@gmail.com>
2018-09-22 20:44:48 +0000

    Scripts second commit

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ for i in `cat IPs.txt`
 do
 echo "Deploying war file into $i QA Server..."
 sleep 6
+pwd
 ssh pass -p "weblogic@123" scp target/*.war mahesh@$i:/home/mahesh/apache-tomcat-8.5.32/webapps
 echo "Starting tomcat server in $i QA Server.."
 sshpass -p "weblogic@123" ssh mahesh@$i "JAVA_HOME=/home/mahesh/jdk1.8.0_171" "/home/mahesh/apache-tomcat-8.5.32/bin/startup.sh"
